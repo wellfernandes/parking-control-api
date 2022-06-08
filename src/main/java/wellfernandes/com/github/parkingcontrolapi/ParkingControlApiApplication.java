844ff0a5eff563939c43d68ee26864f2ff4acfd8
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-08 11:36:20 -0300

    parking spot model created

--- src/main/java/wellfernandes/com/github/parkingcontrolapi/ParkingControlApiApplication.java
+++ src/main/java/wellfernandes/com/github/parkingcontrolapi/ParkingControlApiApplication.java
@@ -2,11 +2,20 @@ package wellfernandes.com.github.parkingcontrolapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApiApplication.class, args);
+	}
+
+	// just to test
+	@GetMapping("/")
+	public String index() {
+		return "running parking control api!";
 	}
 }
