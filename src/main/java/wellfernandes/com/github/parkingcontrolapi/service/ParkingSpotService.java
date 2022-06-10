fcb3e63f1a0f9865feeee80b7e5cb05db417b5f1
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-10 13:33:47 -0300

    get one method (find by id) working

--- src/main/java/wellfernandes/com/github/parkingcontrolapi/service/ParkingSpotService.java
+++ src/main/java/wellfernandes/com/github/parkingcontrolapi/service/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package wellfernandes.com.github.parkingcontrolapi.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,5 +43,9 @@ public class ParkingSpotService {
 
 	public List<ParkingSpotModel> findAll() {
 		return parkingSpotRepository.findAll();
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
 	}
 }
